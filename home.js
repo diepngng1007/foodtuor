1c3b34e6bafbcac0c09cd3931af9dbfe4b8d23ab
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-08-31 22:04:59 +0700

    first commit

--- home.js
+++
@@ -1,6 +0,0 @@
-const headerRight = document.querySelector('.header_right');
-const menuButton = document.querySelector('.fa-bars');
-
-menuButton.addEventListener('click', function() {
-  headerRight.classList.toggle('active');
-});
